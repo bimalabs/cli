dca2954d685cec778062dc709aea2b712c0b29ac
Muhammad Surya Ikhsanuddin <surya.kejawen@gmail.com>
2022-08-04 07:39:55 +0700

    fix missing file

---
+++ bima/bima.go
@@ -0,0 +1,12 @@
+package bima
+
+import "time"
+
+var (
+	Version              = "v1.3.19"
+	ProtocMinVersion     = 31900
+	ProtocGoMinVersion   = 12800
+	ProtocGRpcMinVersion = 10200
+	SpinerIndex          = 9
+	Duration             = 77 * time.Millisecond
+)
